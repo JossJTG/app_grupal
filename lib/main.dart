b7f6f48581256896d4f610cfb77deaa24184a42e
Andree Carlos Arias <andreesebastiancarlosarias@gmail.com>
2023-06-10 21:30:22 -0500

    Texto superior finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,72 @@ class MyApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // Texto - Andree
-
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text('SPIDER',style: TextStyle(
+                      height: 0.9,
+                      fontSize: 70,
+                      fontWeight: FontWeight.w900,
+                      color: Color.fromRGBO(136, 31, 31, 1.0),
+                      shadows: [
+                        Shadow(
+                          color: Color.fromRGBO(90, 102, 125, 1.0),
+                          blurRadius: 10,
+                          offset: Offset(8, 8),
+                        ),
+                      ],
+                      decoration: TextDecoration.none,
+                      fontFamily: 'Arial',
+                      letterSpacing: 4,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Text('MAN',style: TextStyle(
+                      height: 0.9,
+                      fontSize: 70,
+                      fontWeight: FontWeight.w900,
+                      color: Color.fromRGBO(136, 31, 31, 1.0),
+                      shadows: [
+                        Shadow(
+                          color: Color.fromRGBO(90, 102, 125, 1.0),
+                          blurRadius: 10,
+                          offset: Offset(8, 8),
+                        ),
+                      ],
+                      decoration: TextDecoration.none,
+                      fontFamily: 'Arial',
+                      letterSpacing: 4,
+                      ),
+                    ),
+                  ),
+                  
+                  Text('“Un gran poder conlleva una gran responsabilidad”',style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                          color: Color.fromRGBO(90, 102, 125, 1.0),
+                          blurRadius: 3,
+                          offset: Offset(1, 1),
+                        ),
+                        Shadow(
+                          color: Color.fromRGBO(160, 35, 35, 1),
+                          blurRadius: 3,
+                          offset: Offset(-1, -1),
+                        ),
+                      ],
+                      decoration: TextDecoration.none,
+                      letterSpacing: 2,
+                      fontFamily: 'Arial',
+                    ),
+                  ),
+                ],
+              ),
 
               // Imagen - Jean
 
